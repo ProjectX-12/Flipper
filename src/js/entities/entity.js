eb69d8c0d6e21f3d2e67575d25761c70d94ef80a
ProjectX-12 <daniel.projectx12@gmail.com>
2025-02-10 01:38:36 +0000

    Classe da entidade criada

--- src/js/entities/entity.js
+++ src/js/entities/entity.js
@@ -1,14 +1,15 @@
 class Entity{
     constructor(){
-        this.id = Math.random();
-        this.components = {};
+        this.id = Math.floor(Math.random() * 1000000);//Gera um ID aleatório (Arrendonar aqui depois)
+        this.components = {};//Cria um objeto vazio para pode receber os componentes futuramente
     }
 
     addComponent(component){
-        this.components[component.constructor.name] = component;
+        this.components[component.constructor.name/*Busca o nome da classe do componente instanciado para usar como chave*/] = component;
     }
 
     returnComponent(componentName){
         return this.components[componentName];
     } 
 }       
+      
